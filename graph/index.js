c2f8a9f7979c35ccaa56880eb780d57842118a75
mary <noroozi.m90@gmail.com>
2025-07-30 16:19:17 -0400

    graph

---
+++ graph/index.js
@@ -0,0 +1,33 @@
+const graph = {};
+
+graph["you"] = ["Shima", "Nafise", "Ensi"];
+graph["Shima"] = ["Parisa", "Arezoo"];
+graph["Nafise"] = ["Ensi", "Matin"];
+graph["Parisa"] = ["Zahra"];
+graph["Arezoo"] = ["Aida"];
+graph["Ensi"] = [];
+graph["Matin"] = [];
+graph["Zahra"] = [];
+graph["Aida"] = [];
+
+let searchQueue = graph["you"];
+
+let visited = new Set();
+
+while (searchQueue.length !== 0) {
+  const person = searchQueue[0];
+  searchQueue = searchQueue.filter((item, index) => index !== 0);
+  //   searchQueue = searchQueue.slice(1);
+  if (!visited.has(person)) {
+    console.log(person);
+    visited.add(person);
+    console.log(visited, "visited");
+
+    if (person.startsWith("Z")) {
+      console.log(`Ok, ${person} is a Doctor`);
+      return;
+    } else {
+      searchQueue.push(...graph[person]);
+    }
+  }
+}
