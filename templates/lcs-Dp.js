fbf26d9cf778499e87f53efc3e5a21e2232eab83
mary <noroozi.m90@gmail.com>
2025-08-04 21:49:01 -0400

    Longest Common Subsequence (LCS) - DP template

---
+++ templates/lcs-Dp.js
@@ -0,0 +1,20 @@
+// =======================
+// ✅ 8. Longest Common Subsequence (LCS) - DP
+// =======================
+function lcs(s1, s2) {
+  const m = s1.length,
+    n = s2.length;
+  const dp = Array.from({ length: m + 1 }, () => Array(n + 1).fill(0));
+
+  for (let i = 1; i <= m; i++) {
+    for (let j = 1; j <= n; j++) {
+      if (s1[i - 1] === s2[j - 1]) {
+        dp[i][j] = dp[i - 1][j - 1] + 1;
+      } else {
+        dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+      }
+    }
+  }
+
+  return dp[m][n];
+}
