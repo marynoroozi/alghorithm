e5082a9e186c926723d1aee7a0d53cd072bf11eb
mary <noroozi.m90@gmail.com>
2025-08-04 21:46:06 -0400

    binary Search template

---
+++ templates/binarySearch.js
@@ -0,0 +1,15 @@
+// =======================
+// ✅ 3. Binary Search Template
+// Used in: Binary Search, Rotated Array, etc.
+// =======================
+function binarySearch(arr, target) {
+  let left = 0,
+    right = arr.length - 1;
+  while (left <= right) {
+    const mid = Math.floor((left + right) / 2);
+    if (arr[mid] === target) return mid;
+    else if (arr[mid] < target) left = mid + 1;
+    else right = mid - 1;
+  }
+  return -1;
+}
