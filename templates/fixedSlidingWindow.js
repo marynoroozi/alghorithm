3565a44c5616efe1c863fdbe9db8eb1c6503a8cc
mary <noroozi.m90@gmail.com>
2025-08-06 19:26:14 -0400

    fixed sliding window template

---
+++ templates/fixedSlidingWindow.js
@@ -0,0 +1,16 @@
+function maxAverage(nums, k) {
+  let windowSum = 0;
+
+  for (let i = 0; i < k; i++) {
+    windowSum += nums[i];
+  }
+
+  let maxSum = windowSum;
+
+  for (let i = k; i < nums.length; i++) {
+    windowSum = windowSum - nums[i - k] + nums[i];
+    maxSum = Math.max(maxSum, windowSum);
+  }
+
+  return maxSum / k;
+}
