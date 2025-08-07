247dbaa4b468b15993ea7d05c218b3305c8051a1
mary <noroozi.m90@gmail.com>
2025-08-07 15:37:31 -0400

    largest Altitude

---
+++ prefix-sum/largestAltitude.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} gain
+ * @return {number}
+ */
+var largestAltitude = function (gain) {
+  let max = 0;
+  let sum = 0;
+  for (let i = 0; i < gain.length; i++) {
+    sum += gain[i];
+    max = Math.max(max, sum);
+  }
+  return max;
+};
+
+const gain = [-4, -3, -2, -1, 4, 3, 2];
+console.log(largestAltitude(gain));
