0d558c1735f64f5f2406db59456fefb69744ae26
mary <noroozi.m90@gmail.com>
2025-08-13 07:43:24 -0400

    decodeString

--- stack/decodeString.js
+++ stack/decodeString.js
@@ -2,7 +2,26 @@
  * @param {string} s
  * @return {string}
  */
-var decodeString = function (s) {};
+var decodeString = function (s) {
+  let stack = [];
+  for (let ch of s) {
+    if (ch !== "]") {
+      stack.push(ch);
+    } else {
+      let subStr = "";
+      while (stack.length && stack[stack.length - 1] !== "[") {
+        subStr = stack.pop() + subStr;
+      }
+      stack.pop();
+      let k = "";
+      while (stack.length && !isNaN(stack[stack.length - 1])) {
+        k = stack.pop() + k;
+      }
+      stack.push(subStr.repeat(Number(k)));
+    }
+  }
+  return stack.join("");
+};
 
-const s = "3[a]2[bc]";
+const s = "3[a2[c]]";
 console.log(decodeString(s));
