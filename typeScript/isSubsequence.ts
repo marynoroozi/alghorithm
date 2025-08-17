6ef80dcf9e0e900b45e8d3ae9474cbaa6616b9a1
mary <noroozi.m90@gmail.com>
2025-08-17 09:06:04 -0400

    isSubsequence ts

---
+++ typeScript/isSubsequence.ts
@@ -0,0 +1,11 @@
+function isSubsequence(s: string, t: string): boolean {
+  let sIndex = 0;
+  let tIndex = 0;
+  while (sIndex < s.length && tIndex < t.length) {
+    if (s[sIndex] === t[tIndex]) {
+      sIndex++;
+    }
+    tIndex++;
+  }
+  return sIndex === s.length;
+}
