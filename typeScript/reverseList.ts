06424c4a527bfd252d8cf3f5aaea62c47dca36b6
mary <noroozi.m90@gmail.com>
2025-08-18 17:44:15 -0400

    reverseList ts

---
+++ typeScript/reverseList.ts
@@ -0,0 +1,22 @@
+//   Definition for singly-linked list.
+class ListNode {
+  val: number;
+  next: ListNode | null;
+  constructor(val?: number, next?: ListNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.next = next === undefined ? null : next;
+  }
+}
+
+function reverseList(head: ListNode | null): ListNode | null {
+  if (!head) return null;
+  let curr = head;
+  let prev = null;
+  while (curr !== null) {
+    let temp = curr.next;
+    curr.next = prev;
+    prev = curr;
+    curr = temp;
+  }
+  return prev;
+}
