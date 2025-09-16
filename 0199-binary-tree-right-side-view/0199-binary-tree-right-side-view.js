a551665bc558b9fba08398a4b9ce074685e14130
m.noroozi <noroozi.m90@gmail.com>
2025-09-16 11:36:04 -0400

    Time: 0 ms (100%), Space: 54.4 MB (91.44%) - LeetHub

---
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.js
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var rightSideView = function (root) {
+     if (!root) return []; 
+  let queue = [root];
+  let res = [];
+
+  while (queue.length) {
+    let qLen = queue.length
+
+    for (let i = 0; i < qLen; i++) {
+      let node = queue.shift();
+
+      if (node.left) queue.push(node.left);
+      if (node.right) queue.push(node.right);
+              if (i===qLen-1) {
+        res.push(node.val);
+      }
+    
+
+    }
+  }
+  return res;
+};
