3056ff6b918a75e118fb229349dbf84cb3706f75
m.noroozi <noroozi.m90@gmail.com>
2025-09-18 11:36:54 -0400

    Time: 20 ms (89.4%), Space: 66 MB (60.93%) - LeetHub

--- 2413-smallest-number-in-infinite-set/2413-smallest-number-in-infinite-set.js
+++ 2413-smallest-number-in-infinite-set/2413-smallest-number-in-infinite-set.js
@@ -1,27 +1,36 @@
-class SmallestInfiniteSet {
-  constructor() {
-    this.current = 1;
-    this.minHeap = new MinPriorityQueue();
-    this.set = new Set();
-  }
+var SmallestInfiniteSet = function() {
+    this.smallest = 1;      // همیشه نشون میده کوچک‌ترین عدد بعدی چیه
+    this.removed = new Set(); // نگه میداره چه عددهایی pop شدن
+};
 
-  popSmallest() {
-    if (!this.minHeap.isEmpty()) {
-      let smallest = this.minHeap.dequeue();
-      this.set.delete(smallest);
-      return smallest;
+/**
+ * @return {number}
+ */
+SmallestInfiniteSet.prototype.popSmallest = function() {
+    let ret = this.smallest;       // عدد فعلی رو برمی‌داریم
+    this.removed.add(ret);         // نشون میدیم که این عدد دیگه مصرف شد
+    this.smallest += 1;            // میریم سراغ بعدی
+    
+    // تا وقتی که smallest توی removed باشه، میریم جلوتر
+    while (this.removed.has(this.smallest)) {
+        this.smallest += 1;
     }
-    return this.current++;
-  }
 
-  addBack(num) {
-    if (num < this.current && !this.set.has(num)) {
-      this.minHeap.enqueue(num);
-      this.set.add(num);
+    return ret;
+};
+
+/** 
+ * @param {number} num
+ * @return {void}
+ */
+SmallestInfiniteSet.prototype.addBack = function(num) {
+    if (this.removed.has(num)) {
+        this.removed.delete(num);  // دوباره آزادش می‌کنیم
+        if (num < this.smallest) {
+            this.smallest = num;   // اگه کوچیک‌تر از smallest بود، آپدیت می‌کنیم
+        }
     }
-  }
-}
-
+};
 
 /** 
  * Your SmallestInfiniteSet object will be instantiated and called as such:
