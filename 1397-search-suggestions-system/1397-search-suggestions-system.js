0bb911f37346e5ab58113503fe05c7b6aa0ff02c
m.noroozi <noroozi.m90@gmail.com>
2025-09-19 10:31:26 -0400

    Time: 10 ms (94.29%), Space: 65.8 MB (94.29%) - LeetHub

--- 1397-search-suggestions-system/1397-search-suggestions-system.js
+++ 1397-search-suggestions-system/1397-search-suggestions-system.js
@@ -3,31 +3,25 @@
  * @param {string} searchWord
  * @return {string[][]}
  */
-var suggestedProducts = function(products, searchWord) {
-    products.sort();
-    let result = [];
+var suggestedProducts = function (products, searchWord) {
+  products.sort(); // O(nlogn)
+  let l = 0, r = products.length - 1;
+  let result = [];
+  
+  for (let i = 0; i < searchWord.length; i++) {
+    let ch = searchWord[i];
     
-    function lowerBound(arr, target) {
-        let left = 0, right = arr.length;
-        while (left < right) {
-            let mid = Math.floor((left + right) / 2);
-            if (arr[mid] < target) left = mid + 1;
-            else right = mid;
-        }
-        return left;
+    while (l <= r && (products[l].length <= i || products[l][i] !== ch)) l++;
+    while (l <= r && (products[r].length <= i || products[r][i] !== ch)) r--;
+    
+    let suggestions = [];
+    let size = Math.min(3, r - l + 1); // correct count
+    for (let j = 0; j < size; j++) {
+      suggestions.push(products[l + j]);
     }
     
-    let prefix = "";
-    for (let char of searchWord) {
-        prefix += char;
-        
-        // شروع و پایان بازه
-        let start = lowerBound(products, prefix);
-        let end   = lowerBound(products, prefix + '{'); // '{' بعد از 'z'
-        
-        // slice می‌کنیم
-        result.push(products.slice(start, Math.min(start + 3, end)));
-    }
-    
-    return result;
+    result.push(suggestions);
+  }
+  
+  return result;
 };
