c57b250d2e60fcd266196b5b78118465d1293789
m.noroozi <noroozi.m90@gmail.com>
2025-10-21 13:20:57 -0400

    Time: 16 ms (42.16%), Space: 63.9 MB (25.92%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.js
+++ 0207-course-schedule/0207-course-schedule.js
@@ -6,6 +6,7 @@
 var canFinish = function(numCourses, prerequisites) {
     const prereqs = new Map()
 
+    // ساخت گراف از پیش‌نیازها
     for([course, pre] of prerequisites){
         if(!prereqs.has(course)) prereqs.set(course, [])
         prereqs.get(course).push(pre)
@@ -14,6 +15,7 @@ var canFinish = function(numCourses, prerequisites) {
     const seen = new Set()
 
     function hasCycle(course){
+        // اگر در مسیر فعلی هست → چرخه داریم
         if(seen.has(course)) return true
 
         seen.add(course)
@@ -21,10 +23,11 @@ var canFinish = function(numCourses, prerequisites) {
             if(hasCycle(pre)) return true
         }
         seen.delete(course)
-        prereqs.set(course,[])
+        prereqs.set(course,[])   // پاکسازی همه پیش نیازها 
         return false
     }
 
+    // بررسی همه‌ی کورس‌ها
     for(let course=0; course<numCourses; course++){
         if(hasCycle(course)) return false
     }
